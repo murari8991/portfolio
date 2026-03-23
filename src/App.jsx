f378ea56910de94b1018a88e7ee963075a38d5a9
murari8991 <murari2lakshman.ponduru@gmail.com>
2026-03-23 16:45:33 -0500

    Shadow-enhancemnts

--- src/App.jsx
+++ src/App.jsx
@@ -14,15 +14,15 @@ function App() {
         </div>
       </section>
 
-      <section id="about" class="flex flex-col items-center mb-96">
-        <div class="content-card flex flex-col gap-8">
+      <section id="about" class="flex flex-col items-center mb-40">
+        <div class="content-card flex flex-col gap-6">
           <div class="pt-4">
             <h3 class="text-2xl font-bold">About me</h3>
           </div>
           <hr></hr>
           <div class="flex gap-x-3 content">
             <h2 class="text-5xl text-[#ffbd00] font-bold basis-3/12">Hi, name's Murari</h2>
-            <div class="vertical mr-6"></div>
+            <div class="border-solid border-l mr-6"></div>
             <p class="text-lg basis-10/12 font-semibold">
               I am a passionate about learning new things in technology and applying it in real-life.
               Working on projects and building applications that provide value to people is what fuels me. 
@@ -31,19 +31,20 @@ function App() {
             </p>
           </div>
           <div class="flex gap-8 mb-4">
-            <a href="https://www.linkedin.com/in/s-m-v-a-lakshmanacharyulu-ponduru-43b8aa24b" class="p-2 bg-[#ffbb006b] rounded">LinkedIn</a>
-            <a href="https://github.com/Murari-Lakshman" class="px-4 py-2 bg-[#ffbb006b] rounded">Github</a>
-            <a href="https://leetcode.com/u/MxWHMnMB8v/" class="p-2 bg-[#ffbb006b] rounded">Leetcode</a>
+            <a href="https://www.linkedin.com/in/s-m-v-a-lakshmanacharyulu-ponduru-43b8aa24b" class="p-2 bg-[#ffbb006b] rounded-md">LinkedIn</a>
+            <a href="https://github.com/Murari-Lakshman" class="px-4 py-2 bg-[#ffbb006b] rounded-md">Github</a>
+            <a href="https://leetcode.com/u/MxWHMnMB8v/" class="p-2 bg-[#ffbb006b] rounded-md">Leetcode</a>
           </div>
         </div>
       </section>
 
-      <section id="skills" class="flex flex-col mb-96">
-        <div class="content-card">
-          <div>
-            <h3>Skills and Stack</h3>
+      <section id="skills" class="flex flex-col mb-40">
+        <div class="content-card flex flex-col gap-6">
+          <div class="pt-4">
+            <h3 class="text-2xl font-bold">Skills and Stack</h3>
           </div>
-          <ul class="flex flex-col gap-4">
+          <hr></hr>
+          <ul class="flex flex-col gap-4 mb-4">
             <li>
               <h3>Languages: </h3>
               <div class="flex gap-4">
@@ -64,54 +65,83 @@ function App() {
                 <p class="skill-chip">RAG</p>
               </div>
             </li>
-            <li class="content-card"><span>Tools & Methodologies: Git, GitHub, Agile/Scrum, SQL Server, Data Structures, Algorithms</span></li>
-            <li class="content-card"><span>Data Science: </span>Data Pipelines, ETL Processes, Data Engineering, BeautifulSoup, PowerBi, Data Visualization</li>
-          </ul>
-        </div>
-      </section>
-
-      <section id="experience">
-        <div>
-          <div>
-            <h3>My Experience</h3>
-          </div>
-          <ul>
             <li>
-              <div>
-                <h2>Summer Intern</h2>
-              </div>
-              <div>
-              <p>CoreCard, Norcross, Georgia | May 2025 to July 2025</p>
-              <p>
-                Designed a custom MCP for an in-house Gen-AI application that utilizes a RAG.
-                Contributed to modernizing the company's product by developing a new front-end using React and TypeScript.
-                Obfuscated the proprietry source code of using Python to ensure reliablity during training.
-              </p>
+              <h3>Tools & Methodologies:</h3>
+              <div class="flex gap-4">
+                <p class="skill-chip">Git</p>
+                <p class="skill-chip">GitHub</p>
+                <p class="skill-chip">Agile/Scrum</p>
+                <p class="skill-chip">Data Structures</p>
+                <p class="skill-chip">Algorithms</p>
               </div>
             </li>
             <li>
-              <div>
-                <h2>Software Intern</h2>
-              </div>
-              <div>
-              <p>Techsophy Solutions Pvt. Ltd., Hyderabad, India | May 2023 to July 2023</p>
-              <p>
-                Employed Machine Vision and AI to detect cars to secure a community and track the cars going in and out.
-                Fine-tuned YOLO V5 model to ensure proper detection.
-              </p>
+              <h3>Data Science:</h3>
+              <div class="flex gap-4">
+                <p class="skill-chip">Data Pipelines</p>
+                <p class="skill-chip">ETL Pipelines</p>
+                <p class="skill-chip">Data Engineering</p>
+                <p class="skill-chip">BeautifulSoup</p>
+                <p class="skill-chip">PowerBI</p>
+                <p class="skill-chip">Data Visualization</p>
               </div>
             </li>
           </ul>
         </div>
       </section>
 
-      <section id="edu">
-        <div>
-          <div>
-            <h3>Education</h3>
+      <section id="experience" class="flex flex-col mb-40">
+        <div class="content-card flex flex-col gap-6">
+          <div class="pt-4">
+            <h3 class="text-2xl font-bold">My Experience</h3>
           </div>
-          <ul>
-            <li>
+          <hr></hr>
+          <ul class="flex flex-col gap-4 mb-4">
+            <li class="skill-chip">
+              <div class="flex flex-col gap-4 m-4">
+                <div class="flex gap-4 justify-between">
+                  <h2 class="text-[#ffbd00] text-2xl font-bold mt-3">Summer Intern</h2>
+                  <div class="flex flex-col mr-4 items-end">
+                    <p class="italic font-semibold text-lg">CoreCard, Norcross, Georgia</p>
+                    <p class="italic text-lg">May 2025 to July 2025</p>
+                  </div>
+                </div>
+                <hr></hr>
+                <p class="text-lg font-semibold">
+                  Designed a custom MCP for an in-house Gen-AI application that utilizes a RAG.
+                  Contributed to modernizing the company's product by developing a new front-end using React and TypeScript.
+                  Obfuscated the proprietry source code of using Python to ensure reliablity during training.
+                </p>
+              </div>
+            </li>
+            <li class="skill-chip">
+              <div class="flex flex-col gap-4 m-4">
+                <div class="flex gap-4 justify-between">
+                  <h2 class="text-[#ffbd00] text-2xl font-bold mt-3">Software Intern</h2>
+                  <div class="flex flex-col mr-4 items-end">
+                    <p class="italic text-lg font-semibold">Techsophy Solutions, Hyd, IN</p>
+                    <p class="italic text-lg">May 2023 to July 2023</p>
+                  </div>
+                </div>
+                <hr></hr>
+                <p class="text-lg font-semibold">
+                  Employed Machine Vision and AI to detect cars to secure a community and track the cars going in and out.
+                  Fine-tuned YOLO V5 model to ensure proper detection.
+                </p>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </section>
+
+      <section id="edu" class="flex flex-col mb-40">
+        <div class="content-card flex flex-col gap-6">
+          <div class="pt-4">
+            <h3 class="text-2xl font-bold">Education</h3>
+          </div>
+          <hr></hr>
+          <ul class="flex flex-col gap-4 mb-4">
+            <li class="skill-chip">
               <div>
                 <h2>Masters in Information Systems</h2>
               </div>
@@ -125,7 +155,7 @@ function App() {
                 </p>
               </div>
             </li>
-            <li>
+            <li class="skill-chip">
               <div>
                 <h2>Bachelors in Computer Science</h2>
               </div>
@@ -144,13 +174,14 @@ function App() {
         </div>
       </section>
 
-      <section id="projects">
-        <div>
-          <div>
-            <h3>Projects</h3>
+      <section id="projects"  class="flex flex-col mb-16">
+        <div class="content-card flex flex-col gap-6">
+          <div class="pt-4">
+            <h3 class="text-2xl font-bold">Projects</h3>
           </div>
-          <ul>
-            <li>
+          <hr></hr>
+          <ul class="flex flex-col gap-4 mb-4">
+            <li class="skill-chip">
               <div>
                 <h2>ML Model to Find Salary Using Experience</h2>
               </div>
@@ -161,7 +192,7 @@ function App() {
                 </p>
               </div>
             </li>
-            <li>
+            <li class="skill-chip">
               <div>
                 <h2>Web-Crawling and Data Cleaning using Python</h2>
               </div>
@@ -182,7 +213,7 @@ function App() {
         </div>
       </section> */}
 
-      <footer>
+      <footer class="flex items-center justify-center bg-[#550000]">
         <p>email: murari2lakshman.ponduru@gmail.com</p>
       </footer>
     </div>
